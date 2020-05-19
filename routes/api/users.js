c07094cb8ab0a0a484fd41e3a9ae71833c90a447
Prasanga <101890972@student.swin.edu.au>
2020-05-19 21:27:00 +1000

    Added json web token and returns it after user is registered

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const gravatar = require('gravatar')
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 const { check, validationResult } = require('express-validator/check');
-const bcrypt = require('bcryptjs')
+const config = require('config')
+
 
 const User = require('../../models/users')
 
@@ -62,7 +65,20 @@ router.post(
             await user.save();
 
             //return jsonwebtoen
-            res.send('User registered')
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'), { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            )
 
         } catch (error) {
             console.error(err.message);
